0a10a48c60475d8bd09dc5edd8ddcfbd98584e2d
yh <dudghk1207@gmail.com>
2022-08-12 18:04:08 +0900

    maven

--- src/main/java/com/solbox/delivery/ktcloudSDK/App.java
+++ src/main/java/com/solbox/delivery/ktcloudSDK/App.java
@@ -6,13 +6,13 @@ public class App {
 	public static void main(String[] args) {
 		try {
 
-			String confPath = "C:\\Users\\young hwa park\\Desktop\\yhp\\source\\ktcloud\\ktcloud_sdk_maven_1.0.4\\src\\main\\java\\com\\solbox\\delivery\\ktcloudSDK\\conf.json";
+			String confPath = "C:\\Users\\young hwa park\\Desktop\\yhp\\source\\ktcloud\\ktcloud_sdk_1.0.4\\conf.json";
 
 			ServerInformation serverInformation = KTCloudOpenAPI.createServer("nanana", "solbox",confPath);
 			//ServerInformation serverInformation = KTCloudOpenAPI.createServer("nanana",confPath);
 
-			String result = KTCloudOpenAPI.deleteServer(serverInformation, confPath);
-			KTCloudOpenAPI.init(confPath);
+			KTCloudOpenAPI.deleteServer(serverInformation, confPath);
+			//KTCloudOpenAPI.init(confPath);
 
 		} catch (Exception e) {
 			System.out.println(e);
